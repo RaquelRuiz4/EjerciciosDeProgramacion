d9775b3685f6c4c6618ff4ee788af8ec5dd04434
RaquelRuiz4 <raquelruiz121295@gmail.com>
2020-10-03 18:47:03 +0200

    Actualización

--- TemaUno/EjercicioCinco.java
+++ TemaUno/EjercicioCinco.java
@@ -1,12 +1,12 @@
 public class EjercicioCinco {
 public static void main (String [] args) {
-System.out.println( "Lunes    Martes    Miércoles    Jueves    Viernes");
-System.out.println("--------------------------------------------------");
-System.out.printf(" %-8s %-11s %-11s %-9s %-10s\n", "PRO", "SINF", "PRO", "PRO", "SINF"); 
-System.out.printf(" %-8s %-11s %-11s %-9s %-10s\n", "PRO", "SINF", "PRO", "PRO", "SINF");
-System.out.printf(" %-8s %-11s %-11s %-9s %-10s\n", "BBDD", "SINF", "BBDD", "PRO", "SINF");
-System.out.printf(" %-8s %-11s %-11s %-9s %-10s\n", "BBDD", "PRO", "ED", "BBDD", "FOL");
-System.out.printf(" %-8s %-11s %-11s %-9s %-10s\n", "LM", "LM", "ED", "BBDD", "FOL");
-System.out.printf(" %-8s %-11s %-11s %-9s %-10s\n", "LM", "LM", "ED", "BBDD", "FOL");
+System.out.println( "\033[35mLunes    Martes    Miércoles    Jueves    Viernes");
+System.out.println("\033[93m----------\033[32m----------\033[96m----------\033[94m----------\033[95m---------- ");
+System.out.printf(" \033[94m%-8s \033[91m%-11s \033[94m%-11s \033[94m%-9s \033[91m%-10s\n", "PRO", "SINF", "PRO", "PRO", "SINF"); 
+System.out.printf(" \033[94m%-8s \033[91m%-11s \033[94m%-11s \033[94m%-9s \033[91m%-10s\n", "PRO", "SINF", "PRO", "PRO", "SINF");
+System.out.printf(" \033[33m%-8s \033[91m%-11s \033[33m%-11s \033[94m%-9s \033[91m%-10s\n", "BBDD", "SINF", "BBDD", "PRO", "SINF");
+System.out.printf(" \033[33m%-8s \033[94m%-11s \033[92m%-11s \033[33m%-9s \033[96m%-10s\n", "BBDD", "PRO", "ED", "BBDD", "FOL");
+System.out.printf(" \033[95m%-8s \033[95m%-11s \033[92m%-11s \033[33m%-9s \033[96m%-10s\n", "LM", "LM", "ED", "BBDD", "FOL");
+System.out.printf(" \033[95m%-8s \033[95m%-11s \033[92m%-11s \033[33m%-9s \033[96m%-10s\n", "LM", "LM", "ED", "BBDD", "FOL");
 }
 }
