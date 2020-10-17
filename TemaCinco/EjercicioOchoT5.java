98ae0e2f25ea5d96159a5004fbb9d57f2bf4ec8d
RaquelRuiz4 <raquelruiz121295@gmail.com>
2020-10-17 23:56:12 +0200

    Actualización

--- TemaCinco/EjercicioOchoT5.java
+++ TemaCinco/EjercicioOchoT5.java
@@ -2,9 +2,9 @@ public class EjercicioOchoT5{
 public static void main(String [] args){
   int num = 0;
   System.out.println("Tabla de multiplicar");
+  num = Integer.parseInt(System.console().readLine());
   for ( int i = 0; i<=10; i++){
-    num = Integer.parseInt(System.console().readLine());
-    System.out.println(i*num);
+    System.out.println(num + " x" +" "+ i +" "+ "=" + " " + i*num);
   }
 }
 }
