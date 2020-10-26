83e8312469e98bebb1a188a4fb275d7b05509680
RaquelRuiz4 <raquelruiz121295@gmail.com>
2020-10-26 09:55:51 +0100

    Actualización

---
+++ EjerciciosRefuerzo/EjercicioPiramide.java
@@ -0,0 +1,17 @@
+public class EjercicioPiramide{
+  public static void main (String [] args){
+    int num;
+    System.out.print("Introduzca la cantidad de filas por teclado: ");
+    num = Integer.parseInt(System.console().readLine());
+  // Columnas
+    for (int i = num; i >= 1; i--){ 
+    //filas
+      for (int j = 1; j <= i; j++) { 
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
+
+
