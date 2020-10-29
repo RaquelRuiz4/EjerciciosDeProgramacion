aec9e0ebd604826f2e01f20ba2c61cd998170d5d
RaquelRuiz4 <raquelruiz121295@gmail.com>
2020-10-29 23:28:07 +0100

    Actualización

--- EjerciciosRefuerzo/EjercicioRefuerzo7.java
+++ EjerciciosRefuerzo/EjercicioRefuerzo7.java
@@ -27,14 +27,32 @@ public class EjercicioRefuerzo7{
     //altura
     for (int m = 1; m <= num1; m++){
     //espacio
-      for (int n = num1; n >= m; n--){
-        System.out.println(" ");
+      for (int n = num1; n >= m+1; n--){
+        System.out.print(" ");
       }
     //linea 
-        for ( int b = num1; b >= m; b++){
+        for ( int b = num1; b > num1-m; b--){
           System.out.print(m);
   }
   System.out.println();
   }
+  
+  //===============================================================
+  int num2;
+  System.out.println("Introduzca un número para determinar la altura de la pirámide");
+  num2 = Integer.parseInt(System.console().readLine());
+  // altura 
+  for (int z = 0; z < num2; z++){
+  // espacios
+    for (int x = num2; x >= z+2; x--){
+      System.out.print(" ");
+    }
+  // lineas
+  for (int c = num2; c >= num2-z; c--){
+    
+    System.out.print(num2-z);
+  }
+  System.out.println();
+}
 }
 }
