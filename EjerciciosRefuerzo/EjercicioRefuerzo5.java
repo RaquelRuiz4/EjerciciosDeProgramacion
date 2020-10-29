18617a6f75e8398fc2b330acc26a3cab58d1a21f
RaquelRuiz4 <raquelruiz121295@gmail.com>
2020-10-29 09:40:54 +0100

    Actualización

--- EjerciciosRefuerzo/EjercicioRefuerzo5.java
+++ EjerciciosRefuerzo/EjercicioRefuerzo5.java
@@ -1,17 +1,60 @@
 public class EjercicioRefuerzo5{
-  public static void main (String [] args){
+  public static void main(String[] args){
+  
     int num;
-    System.out.print("Introduzca la cantidad de filas por teclado: ");
+
+    System.out.println("Introduzca un número que indicara la altura de la pirámide");
     num = Integer.parseInt(System.console().readLine());
-  // Columnas
-    for (int i = num; i >= 1; i--){ 
-    //filas
-      for (int j = 1; j <= i; j++) { 
-        System.out.print("*");
+  
+    for (int i = 0; i < num; i++){
+  
+      for (int j = 1; j<=i; j++){
+        System.out.print(" ");
       }
-        System.out.println();
+      System.out.print(i+1);
+      System.out.println();
     }
+  
+
+
+//----------------------------------------------------
+
+    int num1;
+    System.out.println("Introduzca un número para indicar la altura de la pirámide");
+    num1 = Integer.parseInt(System.console().readLine());
+        //altura
+      for (int l = 0; l < num1; l++) {
+        // espacio
+        for(int m = 1; m<=l; m++){
+          System.out.print(m);
+           for(int n = num1; n<=m; n++){
+              System.out.print(" ");
+            }
+          }
+        System.out.print(l+1);
+        System.out.println();
+        }
+      
+      
+// ===================================================================
+   int num2;
+    System.out.println("Introduzca un número para indicar la altura de la pirámide");
+    num2 = Integer.parseInt(System.console().readLine());
+        //altura
+      for (int w = 1; w <= num2; w++) {
+        // espacio
+        for(int e = 0; e < w -1 ; e++){
+          System.out.print(" ");
+        }
+        for(int t = num2; t > (num2 - w); t--){
+          System.out.print(t);
+        } 
+        System.out.println();
+      }
+        
+      
   }
 }
+ 
 
-
+    
--- EjerciciosRefuerzo/EjercicioRefuerzo6.java
+++ EjerciciosRefuerzo/EjercicioRefuerzo6.java
@@ -1,60 +1,36 @@
 public class EjercicioRefuerzo6{
-  public static void main(String[] args){
-  
-    int num;
-
-    System.out.println("Introduzca un número que indicara la altura de la pirámide");
-    num = Integer.parseInt(System.console().readLine());
-  
-    for (int i = 0; i < num; i++){
-  
-      for (int j = 1; j<=i; j++){
-        System.out.print(" ");
+  public static void main(String [] args){
+  int num;
+  System.out.println("Introduzca la altura de la pirámide");
+  num = Integer.parseInt(System.console().readLine());
+    for (int i = 0; i <= num; i++) {
+      for (int j= 1; j <= i; j++) {
+        System.out.print(i);
       }
-      System.out.print(i+1);
       System.out.println();
     }
-  
-
-
-//----------------------------------------------------
-
-    int num1;
-    System.out.println("Introduzca un número para indicar la altura de la pirámide");
-    num1 = Integer.parseInt(System.console().readLine());
-        //altura
-      for (int l = 0; l < num1; l++) {
-        // espacio
-        for(int m = 1; m<=l; m++){
-          System.out.print(m);
-           for(int n = num1; n<=m; n++){
-              System.out.print(" ");
-            }
-          }
-        System.out.print(l+1);
-        System.out.println();
-        }
       
-      
-// ===================================================================
-   int num2;
-    System.out.println("Introduzca un número para indicar la altura de la pirámide");
-    num2 = Integer.parseInt(System.console().readLine());
-        //altura
-      for (int w = 1; w <= num2; w++) {
-        // espacio
-        for(int e = 0; e < w -1 ; e++){
-          System.out.print(" ");
-        }
-        for(int t = num2; t > (num2 - w); t--){
-          System.out.print(t);
-        } 
-        System.out.println();
-      }
-        
-      
+// ====================================================================
+
+  int num2;
+  System.out.println("Introduzca la altura de la pirámide");
+  num2 = Integer.parseInt(System.console().readLine());
+  for (int z = 0; z < num2; z++){
+    for (int j = num2; j >= (num2 -z); j--){
+      System.out.print(num2-z);
+    }
+    System.out.println();
+  }
+
+ // ==========================================================
+  int num1;
+  System.out.println("Introduzca la altura de la pirámide");
+  num1 = Integer.parseInt(System.console().readLine());
+  for(int m = 0; m <= num1; m++){
+    for(int n = 1; n <= m; n++) {
+      System.out.print(n);
+    }
+    System.out.println();
   }
 }
- 
-
-    
+}
