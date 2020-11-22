ea566ea0353bddff3ce42f068fd098ae17813ff6
RaquelRuiz4 <raquelruiz121295@gmail.com>
2020-11-22 17:17:38 +0100

    Actualización

---
+++ Tema7/Ejercicio2T7.java
@@ -0,0 +1,15 @@
+public class Ejercicio2T7{
+  public static void main(String[] args) {
+  char[] symbol = new char[10];
+symbol[0] = 'a';
+symbol[1] = 'x';
+symbol[4] = '@';
+symbol[6] = ' ';
+symbol[7] = '+';
+symbol[8] = 'Q';
+System.out.println("El array caracter contiene los siguientes elementos:");
+for (int i = 0; i < 10; i++) {
+System.out.println(symbol[i]);
+}
+}
+}
