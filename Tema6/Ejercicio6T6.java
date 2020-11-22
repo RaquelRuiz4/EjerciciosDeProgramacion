782d02a92e974f3147ff0d8138dc81bf93b1ec54
RaquelRuiz4 <raquelruiz121295@gmail.com>
2020-11-22 13:04:58 +0100

    Actualización

---
+++ Tema6/Ejercicio6T6.java
@@ -0,0 +1,31 @@
+public class Ejercicio6T6{
+  public static void main (String [] args) {
+  int num;
+  int intento = 0;
+  int contador = 0;
+  num = (int) (Math.random()*100);
+  System.out.println(num);
+  do {
+    System.out.println("Adivine un número al azar entre 0 y 100, tiene " + (5 - contador) + " oportunidades");
+    // num = (int) (Math.random()*100);
+
+    intento = Integer.parseInt(System.console().readLine());
+    contador++;
+    if (intento == num){
+    System.out.println("¡Lo has acertado!");
+    }
+    else if (intento < num) {
+      System.out.println("El número introducido es menor!");
+    }
+      else if (intento > num) {
+      System.out.println("El número introducido es mayor!");
+}
+}
+    while ((contador < 5) && (!( num == intento)));
+    if (contador == 5){
+    System.out.println("Lo siento, has agotado las oportunidades"); }
+      
+    
+}
+  }
+
