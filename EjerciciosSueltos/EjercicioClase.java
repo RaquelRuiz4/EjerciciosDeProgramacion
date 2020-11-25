90fbbf2c44e1f23ca5791afeeb7267e3006f06fe
RaquelRuiz4 <raquelruiz121295@gmail.com>
2020-11-25 10:05:02 +0100

    Actualización

---
+++ EjerciciosSueltos/EjercicioClase.java
@@ -0,0 +1,16 @@
+public class EjercicioClase{
+  public static void main(String [] args){
+
+  String[] array1 = new String [5];
+  String[] array2 = new String [8];
+  String[] array3 = new String [13];
+  
+  for (int i = 0; i < 5; i++){
+ array1[i] = System.console().readLine();
+  }
+  
+   for (int j = 0; j< 8;j++){
+    array2[j] = System.console().readLine();
+  }
+}
+}
