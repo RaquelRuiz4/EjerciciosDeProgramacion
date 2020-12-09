474bf5b50a67194af39bcedb029497e9cbf022ae
RaquelRuiz4 <raquelruiz.alu@iescampanillas.com>
2020-12-09 08:28:58 +0100

    Actualización

--- Tema6/Ejercicio17T6.java
+++ Tema6/Ejercicio17T6.java
@@ -6,24 +6,13 @@ la pecera hay que colocar de forma aleatoria un pececito, que puede estar
 situado en cualquiera de las posiciones que quedan en el hueco que forma el
 rectángulo. */
 
-public class Ejercicio17T6{
+public class Ejercicio 17T6{
   public static void main(String[] args){
-  
-  int alto;
-  int ancho;
-  
-  System.out.println("Introduzca el alto de la pecera (min un 4)");
-  alto = Integer.parseInt(System.console().readLine());
-  System.out.println("Introduzca el ancho de la pecera (min un 4)");
-  ancho = Integer.parseInt(System.console().readLine());
-
-for (int i = 0; i <= ancho; i++){
-  System.out.print("*");
-}
-Syste.out.println();
-for (int j = 2; j <= alto; j++){
-}
-}
-}
-}
-
+  String pececito = "@";
+  System.out.println("Introduzca el alto de la pecera ");
+  int alto = Integer.parseInt(System.console().readLine());
+  System.out.println("Introduzca el ancho de la pecera ");
+  int ancho = Integer.parseInt(System.console().readLine());
+  int num = (int) (Math.random()*ancho-alto+1)+alto;
+  }
+  }
