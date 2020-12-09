85c544d95a0e5b8355be93606f9c07442828e353
RaquelRuiz4 <raquelruiz.alu@iescampanillas.com>
2020-12-09 08:35:07 +0100

    Actualización

---
+++ Tema7/Ejercicio5T7.java
@@ -0,0 +1,16 @@
+/* Escribe un programa que pida 10 números por teclado y que luego muestre los
+números introducidos junto con las palabras “máximo” y “mínimo” al lado del
+máximo y del mínimo respectivamente. */
+
+public class Ejercicio5T7{
+  public static void main(String[] args){
+  
+  
+  
+  
+  
+  
+  
+  
+  }
+  }
