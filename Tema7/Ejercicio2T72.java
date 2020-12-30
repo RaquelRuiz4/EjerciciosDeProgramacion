b7d96196d0975864ba8b4591308962ac0644e940
RaquelRuiz4 <raquel.ruiz.alu@iescampanillas.com>
2020-12-30 20:52:30 +0100

    Actualización

--- Tema7/Ejercicio2T72.java
+++
@@ -1,3 +0,0 @@
-public class Ejercicio2T72{
-    public static void main(String[] args){
-    int [][] array = new int [3][6];
--- Tema7/Ejercicio4T7.java
+++ Tema7/Ejercicio4T7.java
@@ -1,19 +1,32 @@
+/* Define tres arrays de 20 números enteros cada una, con nombres numero, cuadrado
+y cubo. Carga el array numero con valores aleatorios entre 0 y 100. En el array
+cuadrado se deben almacenar los cuadrados de los valores que hay en el array
+numero. En el array cubo se deben almacenar los cubos de los valores que hay en
+numero. A continuación, muestra el contenido de los tres arrays dispuesto en tres
+columnas. */
+
 public class Ejercicio4T7{
-  public static void main (String[] args){
-  int i;
-  int [] num = new int [20];
-  int [] cuadrado = new int [20];
-  int [] cubo = new int [20];
+  public static void main(String[] args){
   
-  for (i = 0; i < 20; i++) {
-num[i] = (int)(Math.random()*101);
-cuadrado[i] = num[i] * num[i];
-cubo[i] = num[i] * num[i] * num[i];
-}
+  int[] numero = new int [20];
+  int[] cuadrado = new int [20];
+  int[] cubo = new int [20];
 
-for (i = 0; i < 20; i++) {
-System.out.println("número: " + num[i] + "    " + "cuadrado: " + cuadrado[i] + "    " + "cubo: " + cubo[i]);
+  for(int i = 0; i < 20; i++){
+    numero[i] = (int)(Math.random()*101);
+    System.out.print(numero[i]+ " ");
+  }
+   System.out.println();
+   for(int j = 0; j < 20; j++){
+    cuadrado[j] = (numero[j]*numero[j]);
+    System.out.print(cuadrado[j]+ " ");
+  }
+  System.out.println();
+  for(int k = 0; k < 20; k++){
+    cubo[k] = (numero[k]*numero[k]*numero[k]);
+    System.out.print(cubo[k] + " ");
+  } 
+  
+  }
 }
-}
-}
-
+  
