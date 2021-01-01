108f3b2b7f21118d32b41f51617f807b84b336f9
RaquelRuiz4 <raquel.ruiz.alu@iescampanillas.com>
2021-01-01 21:40:43 +0100

    Actualización

--- Tema7/Ejercicio6T7.java
+++ Tema7/Ejercicio6T7.java
@@ -4,15 +4,27 @@ debe pasar a la posición 1, el de la 1 a la 2, etc. El número que se encuentra
 la última posición debe pasar a la posición 0. Finalmente, muestra el contenido
 del array. */
 
-public class Ejercicio5T7{
+public class Ejercicio6T7{
   public static void main(String[] args){
-  int i;
-  int [] array = new int [10];
-  for (i = 0; i < 15; i++){
-    array[i] = Integer.parseInt(System.console().readLine());
-    System.out.print(array[i]);
-  
-  
+    int i;
+    int [] array = new int [15];
+    System.out.println("Introduzca 15 números: ");
+    for (i = 0; i <= 14; i++){
+      array[i] = Integer.parseInt(System.console().readLine());
+    }
+    System.out.println();
+    // aux para guardar la últ y for para cambiar las posiciones del array
+    int aux = array[14];
+    System.out.println("aux: " + aux);
+    for (i = 14; i > 0; i--){
+      array[i] = array[i-1];
+    }
+    array[0] = aux;
+    // for para printear 
+    for (i = 0; i < 15; i++){
+      System.out.print(array[i] + " ");
+    
     }
   }
 }
+
