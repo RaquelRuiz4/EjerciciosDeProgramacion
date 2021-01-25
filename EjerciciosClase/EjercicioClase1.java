b5da7c34b13c6f66a94f352210a37da810e40ece
RaquelRuiz4 <raquel.ruiz.alu@iescampanillas.com>
2021-01-25 08:48:04 +0100

    Actualización

---
+++ EjerciciosClase/EjercicioClase1.java
@@ -0,0 +1,15 @@
+/* Dada una matriz, preséntese por filas y columnas, hacia adelante y hacia atrás.
+El programa ha de rellenar la matriz con valores aleatorios igualmente probables entre 2
+valores introducidos por el usuario. ambos incluidos. */
+
+public class EjercicioClase1{
+  public static void main(String[] args){
+  
+  public static void  RellenaMatriz (int[][] matriz, int min, int max)
+  
+  for (int i = 0; i < matriz; i++){
+  int[][] = (int) (Math.random()*max-min+1)+min)
+  }
+  
+  }
+}
