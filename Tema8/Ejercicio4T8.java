1115ea6ded64753f21eaf87de92584386da2e77a
RaquelRuiz4 <raquel.ruiz.alu@iescampanillas.com>
2021-01-25 20:56:27 +0100

    Actualización

--- Tema8/Ejercicio4T8.java
+++ Tema8/Ejercicio4T8.java
@@ -4,12 +4,15 @@
   public static void main(String[] args){
     
     int base = 2; 
-    int exp = 4;
+    int exp = 0;
     System.out.println(potencia(base, exp));
   }
   
   public static int potencia (int base, int exp){
     int resultado = base;
+    if ( exp == 0){
+      return 1;
+    }
     for (int i = 1; i < exp; i++){
       resultado *= base;
     }
