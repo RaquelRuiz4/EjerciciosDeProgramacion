654ac8fd4c3432f64adbfd0fbea0e0fa80eae70f
RaquelRuiz4 <raquel.ruiz.alu@iescampanillas.com>
2021-02-10 20:59:05 +0100

    Actualización

---
+++ Tema8/Ejercicio9T8.java
@@ -0,0 +1,11 @@
+// quitaPorDetras: Le quita a un número n dígitos por detrás (por la derecha) 
+ 
+public class Ejercicio9T8{
+  public static void main(String[] args){
+    
+    int prueba = 1234;
+    System.out.println(quitaPorDetras(prueba);
+  }
+  
+  
+  public static int quitaPorDetras (int num);
